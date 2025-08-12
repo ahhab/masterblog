31f4fc67e85df336e76527e56f89e71b5b68e53d
elektrox <elektrox@gmail.com>
2025-08-12 00:53:41 +0000

    posts and route /

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Load posts from JSON file
+with open('posts.json', 'r') as f:
+    blog_posts = json.load(f)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
