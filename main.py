fa23c39b8a7b617e9fd06eb07da12a19cad301b7
elektrox <elektrox@gmail.com>
2025-08-12 01:06:57 +0000

    delete

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ def index():
 def add():
     if request.method == 'POST':
         posts = get_posts()
+        max_id = max(post['id'] for post in posts) if posts else 0
         new_post = {
-            "id": len(posts) + 1,
+            "id": max_id + 1,
             "author": request.form.get('author'),
             "title": request.form.get('title'),
             "content": request.form.get('content')
@@ -30,6 +31,13 @@ def add():
         save_posts(posts)
         return redirect(url_for('index'))
     return render_template('add.html')
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    posts = get_posts()
+    posts = [post for post in posts if post['id'] != post_id]
+    save_posts(posts)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
